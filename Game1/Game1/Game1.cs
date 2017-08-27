9fd8ba1a24dabc580d094ee404a0932ee5b8e853
MisterShake2099 <mistershake2099@gmail.com>
2017-08-27 16:33:32 -0700

    added rotating arrow and circulating, blending color spots

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -12,14 +13,34 @@ namespace Game1
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
+		/*
 		private Texture2D background;
 		private Texture2D shuttle;
 		private Texture2D earth;
-
+		*/
+		/*
 		private SpriteFont scoreFont;
 		private int score = 0;
+		*/
+		/*
+		private Texture2D arrow;
+		private float angle = 0;
+		*/
+		//private AnimatedSprite animatedSprite;
 
-		private AnimatedSprite animatedSprite;
+		private Texture2D red;
+		private Texture2D green;
+		private Texture2D blue;
+
+		private float blueAngle = 0;
+		private float greenAngle = 0;
+		private float redAngle = 0;
+
+		private float blueSpeed = 0.025f;
+		private float greenSpeed = 0.017f;
+		private float redSpeed = 0.022f;
+
+		private float distance = 100;
 
 		public Game1()
 		{
@@ -33,8 +54,6 @@ namespace Game1
 		/// and initialize them as well.
 		protected override void Initialize()
 		{
-
-
 			base.Initialize();
 		}
 
@@ -54,8 +73,14 @@ namespace Game1
 
 			//scoreFont = Content.Load<SpriteFont>("Score");
 
-			Texture2D texture = Content.Load<Texture2D>("Atlases/SmileyWalk");
-			animatedSprite = new AnimatedSprite(texture, 4, 4);
+			//Texture2D texture = Content.Load<Texture2D>("Atlases/SmileyWalk");
+			//animatedSprite = new AnimatedSprite(texture, 4, 4);
+
+			//arrow = Content.Load<Texture2D>("Images/arrow");
+
+			red = Content.Load<Texture2D>("Images/red");
+			green = Content.Load<Texture2D>("Images/green");
+			blue = Content.Load<Texture2D>("Images/blue");
 		}
 
 		/// <summary>
@@ -81,7 +106,13 @@ namespace Game1
 
 			//score++;
 
-			animatedSprite.Update();
+			//animatedSprite.Update();
+
+			//angle += 0.01f;
+
+			blueAngle += blueSpeed;
+			greenAngle += greenSpeed;
+			redAngle += redSpeed;
 
 			base.Update(gameTime);
 		}
@@ -92,20 +123,42 @@ namespace Game1
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
+			GraphicsDevice.Clear(Color.Black);
+
+			Vector2 bluePosition = new Vector2(
+							(float)Math.Cos(blueAngle) * distance,
+							(float)Math.Sin(blueAngle) * distance);
+			Vector2 greenPosition = new Vector2(
+							(float)Math.Cos(greenAngle) * distance,
+							(float)Math.Sin(greenAngle) * distance);
+			Vector2 redPosition = new Vector2(
+							(float)Math.Cos(redAngle) * distance,
+							(float)Math.Sin(redAngle) * distance);
+
+			Vector2 center = new Vector2(300, 140);
+
+			spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Additive);
 
 			/*
-			spriteBatch.Begin();
-
 			spriteBatch.Draw(background, new Rectangle(0, 0, 800, 480), Color.White);
 			spriteBatch.Draw(shuttle, new Vector2(450, 240), Color.White);
 			spriteBatch.Draw(earth, new Vector2(400, 240), Color.White);
-
-			spriteBatch.DrawString(scoreFont, "Score: " + score, new Vector2(100, 100), Color.Black);
-			spriteBatch.End();
+			*/
+			//spriteBatch.DrawString(scoreFont, "Score: " + score, new Vector2(100, 100), Color.Black);
+			/*
+			Vector2 location = new Vector2(400, 240);
+			Rectangle sourceRectangle = new Rectangle(0, 0, arrow.Width, arrow.Height);
+			Vector2 origin = new Vector2(arrow.Width / 2, arrow.Height);
+			spriteBatch.Draw(arrow, location, sourceRectangle, Color.White, angle, origin, 1.0f, SpriteEffects.None, 1);
 			*/
 
-			animatedSprite.Draw(spriteBatch, new Vector2(400, 200));
+			spriteBatch.Draw(blue, center + bluePosition, Color.White);
+			spriteBatch.Draw(green, center + greenPosition, Color.White);
+			spriteBatch.Draw(red, center + redPosition, Color.White);
+
+			spriteBatch.End();
+
+			//animatedSprite.Draw(spriteBatch, new Vector2(400, 200));
 
 			base.Draw(gameTime);
 		}
