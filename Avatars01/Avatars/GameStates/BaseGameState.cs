86bfb34fecf5a53e7d9aaaeb9d917c7f60f4bc17
Doug Snyder <mistershake2099@gmail.com>
2017-09-02 19:27:54 -0700

    Tracking my progress through A Summoner's Tale from gameprogrammingadventures.org

---
+++ Avatars01/Avatars/GameStates/BaseGameState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace Avatars.GameStates
+{
+    public class BaseGameState : GameState
+    {
+        #region Field Region
+
+        protected static Random random = new Random();
+
+        protected Game1 GameRef;
+
+        #endregion
+
+        #region Constructor Region
+
+        public BaseGameState(Game game)
+            : base(game)
+        {
+            GameRef = (Game1)game;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+
+        #endregion
+    }
+}
