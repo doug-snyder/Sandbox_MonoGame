5588a5df949f586a9dc5cf538c9fef9b4dcc0033
Doug Snyder <mistershake2099@gmail.com>
2017-09-24 01:57:43 -0700

    added some images, set up some classes

--- summonerTale/summonerTale/Game1.cs
+++ summonerTale/summonerTale/Game1.cs
@@ -4,22 +4,29 @@ using Microsoft.Xna.Framework.Input;
 
 namespace summonerTale
 {
-	/// <summary>
-	/// This is the main type for your game.
-	/// </summary>
 	public class Game1 : Game
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 
+		Rectangle screenRectangle;
+
 		public Game1()
 		{
 			graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
+
+			graphics.PreferredBackBufferWidth = 750;
+			graphics.PreferredBackBufferHeight = 600;
+
+			screenRectangle = new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
 		}
 
 		protected override void Initialize()
 		{
+			var form = (System.Windows.Forms.Form)System.Windows.Forms.Control.FromHandle(this.Window.Handle);
+			form.Location = new System.Drawing.Point(600, 350);
+
 			base.Initialize();
 		}
 
