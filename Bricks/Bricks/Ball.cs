94c7fbe80f99bfe35df1930dce1df3fecc9c0ca4
Doug Snyder <mistershake2099@gmail.com>
2017-10-04 20:11:35 -0700

    added scoring, text, ball count

---
+++ Bricks/Bricks/Ball.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Bricks
+{
+	class Ball
+	{
+		public float X { get; set; }
+		public float Y { get; set; }
+		public float XVelocity { get; set; }
+		public float YVelocity { get; set; }
+		public float Height { get; set; }
+		public float Width { get; set; }
+		public float Rotation { get; set; }
+		public bool UseRotation { get; set; }
+		public float ScreenWidth { get; set; }
+		public float ScreenHeight { get; set; }
+		public bool Visible { get; set; }
+		public int Score { get; set; }
+		public int BricksCleared { get; set; }
+
+		private Texture2D ImgBall { get; set; }
+		private SpriteBatch spriteBatch;
+		private GameContent gameContent;
+
+		public Ball(float screenWidth, float screenHeight, SpriteBatch spriteBatch, GameContent gameContent)
+		{
+			X = 0;
+			Y = 0;
+			XVelocity = 0;
+			YVelocity = 0;
+			Rotation = 0;
+			ImgBall = gameContent.ImgBall;
+			Width = ImgBall.Width;
+			Height = ImgBall.Height;
+			this.spriteBatch = spriteBatch;
+			this.gameContent = gameContent;
+			ScreenWidth = screenWidth;
+			ScreenHeight = screenHeight;
+			Visible = false;
+			Score = 0;
+			BricksCleared = 0;
+			UseRotation = true;
+		}
+
+		public void Draw()
+		{
+			if (Visible == false)
+			{
+				return;
+			}
+
+			if (UseRotation)
+			{
+				Rotation += .1f;
+				if (Rotation > 3 * Math.PI)
+				{
+					Rotation = 0;
+				}
+			}
+
+			spriteBatch.Draw(ImgBall, new Vector2(X, Y), null, Color.White, Rotation, new Vector2(Width / 2, Height / 2), 1.0f, SpriteEffects.None, 0);
+		}
+
+		public void Launch(float x, float y, float xVelocity, float yVelocity)
+		{
+			if (Visible == true)
+			{
+				return;
+			}
+			PlaySound(gameContent.StartSound);
+			Visible = true;
+			X = x;
+			Y = y;
+			XVelocity = xVelocity;
+			YVelocity = yVelocity;
+		}
+
+		public bool Move(Wall wall, Paddle paddle)
+		{
+			if (Visible == false)
+			{
+				return false;
+			}
+
+			X = X + XVelocity;
+			Y = Y + YVelocity;
+
+			// Wall hits
+			if (X < 1)
+			{
+				X = 1;
+				XVelocity = XVelocity * -1;
+				PlaySound(gameContent.WallBounceSound);
+			}
+			if (X > ScreenWidth - Width + 5)
+			{
+				X = ScreenWidth - Width + 5;
+				XVelocity = XVelocity * -1;
+				PlaySound(gameContent.WallBounceSound);
+			}
+			if (Y < 1)
+			{
+				Y = 1;
+				YVelocity = YVelocity * -1;
+				PlaySound(gameContent.WallBounceSound);
+			}
+			if (Y + Height > ScreenHeight)
+			{
+				Visible = false;
+				Y = 0;
+				PlaySound(gameContent.MissSound);
+				return false;
+			}
+
+			// Paddle hits
+			Rectangle paddleRect = new Rectangle((int)paddle.X, (int)paddle.Y, (int)paddle.Width, (int)paddle.Height);
+			Rectangle ballRect = new Rectangle((int)X, (int)Y, (int)Width, (int)Height);
+			if (HitTest(paddleRect, ballRect))
+			{
+				PlaySound(gameContent.PaddleBounceSound);
+				int offset = Convert.ToInt32((paddle.Width - (paddle.X + paddle.Width - X + Width / 2)));
+
+				offset = offset / 5;
+				if (offset < 0)
+				{
+					offset = 0;
+				}
+
+				switch (offset)
+				{
+					case 0:
+						XVelocity = -6;
+						break;
+					case 1:
+						XVelocity = -5;
+						break;
+					case 2:
+						XVelocity = -4;
+						break;
+					case 3:
+						XVelocity = -3;
+						break;
+					case 4:
+						XVelocity = -2;
+						break;
+					case 5:
+						XVelocity = -1;
+						break;
+					case 6:
+						XVelocity = 1;
+						break;
+					case 7:
+						XVelocity = 2;
+						break;
+					case 8:
+						XVelocity = 3;
+						break;
+					case 9:
+						XVelocity = 4;
+						break;
+					case 10:
+						XVelocity = 5;
+						break;
+					default:
+						XVelocity = 6;
+						break;
+				}
+
+				YVelocity = YVelocity * -1;
+				Y = paddle.Y - Height + 1;
+				return true;
+			}
+
+			bool hitBrick = false;
+			for (int i = 0; i < 7; i++)
+			{
+				if (hitBrick == false)
+				{
+					for (int j = 0; j < 10; j++)
+					{
+						Brick brick = wall.BrickWall[i, j];
+						if (brick.Visible)
+						{
+							Rectangle brickRect = new Rectangle((int)brick.X, (int)brick.Y, (int)brick.Width, (int)brick.Height);
+							if (HitTest(ballRect, brickRect))
+							{
+								PlaySound(gameContent.BrickSound);
+								brick.Visible = false;
+								Score = Score + 7 - i;
+								YVelocity = YVelocity * -1;
+								BricksCleared++;
+								hitBrick = true;
+								break;
+							}
+						}
+					}
+				}
+			}
+			return true;
+		}
+
+		public static bool HitTest(Rectangle r1, Rectangle r2)
+		{
+			if (Rectangle.Intersect(r1, r2) != Rectangle.Empty)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public static void PlaySound(SoundEffect sound)
+		{
+			float volume = 1;
+			float pitch = 0.0f;
+			float pan = 0.0f;
+			sound.Play(volume, pitch, pan);
+		}
+
+	}
+}
--- Bricks/Bricks/Game1.cs
+++ Bricks/Bricks/Game1.cs
@@ -13,8 +13,15 @@ namespace Bricks
 		private Paddle paddle;
 		private Wall wall;
 		private GameBorder gameBorder;
+		private Ball ball;
+		private Ball staticBall;
+
 		private int screenWidth = 0;
 		private int screenHeight = 0;
+		private MouseState oldMouseState;
+		private KeyboardState oldKeyboardState;
+		private bool readyToServeBall = true;
+		private int ballsRemaining = 3;
 
 
 		public Game1()
@@ -59,19 +66,65 @@ namespace Bricks
 			int paddleY = screenHeight - 100;													// paddle 100 pixels from bottom
 			paddle = new Paddle(paddleX, paddleY, screenWidth, spriteBatch, gameContent);		// create paddle
 			wall = new Wall(1, 50, spriteBatch, gameContent);									// create wall
-			gameBorder = new GameBorder(screenWidth, screenHeight, spriteBatch, gameContent);	// create game border
+			gameBorder = new GameBorder(screenWidth, screenHeight, spriteBatch, gameContent);   // create game border
+			ball = new Ball(screenWidth, screenHeight, spriteBatch, gameContent);
 
-			gameContent = new GameContent(Content);
+			staticBall = new Ball(screenWidth, screenHeight, spriteBatch, gameContent)			// for indicating 'balls left'
+			{
+				X = 25,
+				Y = 25,
+				Visible = true,
+				UseRotation = false
+			};
 		}
 
 		protected override void UnloadContent(){}
 
 		protected override void Update(GameTime gameTime)
 		{
+			if (IsActive == false)
+			{
+				return;		// window is not yet active — don't update
+			}
+
 			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
 			{
 				Exit();
 			}
+
+			KeyboardState newKeyboardState = Keyboard.GetState();
+			MouseState newMouseState = Mouse.GetState();
+
+			// mouse events
+			if (oldMouseState.X != newMouseState.X)
+			{
+				if (newMouseState.X >= 0 || newMouseState.X < screenHeight)
+				{
+					paddle.MoveTo(newMouseState.X);
+				}
+			}
+			if (newMouseState.LeftButton == ButtonState.Released && oldMouseState.LeftButton == ButtonState.Pressed
+					&& oldMouseState.X == newMouseState.X && oldMouseState.Y == newMouseState.Y && readyToServeBall)
+			{
+				ServeBall();
+			}
+
+			// keyboard events
+			if (newKeyboardState.IsKeyDown(Keys.Left) || newKeyboardState.IsKeyDown(Keys.A))
+			{
+				paddle.MoveLeft();
+			}
+			if (newKeyboardState.IsKeyDown(Keys.Right) || newKeyboardState.IsKeyDown(Keys.D))
+			{
+				paddle.MoveRight();
+			}
+			if (oldKeyboardState.IsKeyUp(Keys.Space) && newKeyboardState.IsKeyDown(Keys.Space) && readyToServeBall)
+			{
+				ServeBall();
+			}
+
+			oldMouseState = newMouseState;
+			oldKeyboardState = newKeyboardState;
 
 			base.Update(gameTime);
 		}
@@ -85,9 +138,66 @@ namespace Bricks
 			wall.Draw();
 			gameBorder.Draw();
 
+			if (ball.Visible)
+			{
+				bool inPlay = ball.Move(wall, paddle);
+				if (inPlay)
+				{
+					ball.Draw();
+				}
+				else
+				{
+					ballsRemaining--;
+					readyToServeBall = true;
+				}
+			}
+			staticBall.Draw();
+
+			string scoreMsg = "Score: " + ball.Score.ToString("00000");
+			Vector2 space = gameContent.LabelFont.MeasureString(scoreMsg);
+			spriteBatch.DrawString(gameContent.LabelFont, scoreMsg, new Vector2((screenWidth - space.X) / 2, screenHeight - 40), Color.White);
+			if (ball.BricksCleared >= 70)
+			{
+				ball.Visible = false;
+				ball.BricksCleared = 0;
+				wall = new Wall(1, 50, spriteBatch, gameContent);
+				readyToServeBall = true;
+			}
+			if (readyToServeBall)
+			{
+				if (ballsRemaining > 0)
+				{
+					string startMsg = "Press <Space> or Click Mouse to Start";
+					Vector2 startSpace = gameContent.LabelFont.MeasureString(startMsg);
+					spriteBatch.DrawString(gameContent.LabelFont, startMsg, new Vector2((screenWidth - startSpace.X) / 2, screenHeight / 2), Color.White);
+				}
+				else
+				{
+					string endMsg = "Game Over";
+					Vector2 endSpace = gameContent.LabelFont.MeasureString(endMsg);
+					spriteBatch.DrawString(gameContent.LabelFont, endMsg, new Vector2((screenWidth - endSpace.X) / 2, screenHeight / 2), Color.White);
+				}
+			}
+			spriteBatch.DrawString(gameContent.LabelFont, ballsRemaining.ToString(), new Vector2(40, 10), Color.White);
+
 			spriteBatch.End();
 
 			base.Draw(gameTime);
+		}
+
+		private void ServeBall()
+		{
+			if (ballsRemaining < 1)
+			{
+				ballsRemaining = 3;
+				ball.Score = 0;
+				wall = new Wall(1, 50, spriteBatch, gameContent);
+			}
+
+			readyToServeBall = false;
+			float ballX = paddle.X + (paddle.Width) / 2;
+			float ballY = paddle.Y - ball.Height;
+			ball.Launch(ballX, ballY, -3, -3);
 		}
 
 	}
